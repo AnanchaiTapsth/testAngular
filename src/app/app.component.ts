26935631d7defc5fb8bdb087300d7cc4453da7e2
chantatha.p <chantaha.p@gmail.com>
2023-05-23 12:02:07 +0700

    init code for interview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'interview_test';
+
+  searchText: string = '';
+  products: any[] = [
+    {
+      name: 'Product 1',
+      description: 'Description of Product 1',
+      price: 9.99,
+      image: 'product1.jpg'
+    },
+    {
+      name: 'Product 2',
+      description: 'Description of Product 2',
+      price: 19.99,
+      image: 'product2.jpg'
+    },
+    // Add more sample product data
+  ];
+
+  ngOnInit() {}
+
+  get filteredProducts(): any[] {
+    if (!this.searchText) {
+      return this.products;
+    }
+    return this.products.filter(product =>
+      product.name.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
 }
